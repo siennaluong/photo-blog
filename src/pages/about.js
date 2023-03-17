320c2dff83080cf5b2b38617fe07a4cffb7da7f6
Sienna Luong <74251221+siennaluong@users.noreply.github.com>
2023-03-17 11:27:59 -0400

    try again

--- src/pages/about.js
+++ src/pages/about.js
@@ -58,7 +58,7 @@ const AboutPage = ({ data }) => {
           ranging from AI project to a unity game. Please shoot me an email if you wish to chat with me about anything tech-related!
         </p>
        <p>I have a big passion for learning new skills and knowledge. I find the process of mastering something new is very rewarding! My main interests are in computer science and engineer,
-        history and cognitive/neuroscience. I even have a bucket list for things I want to learn more about, you can check it out <a href={siennaswriting.netlify.app/learn}>here</a>.
+        history and cognitive/neuroscience. 
         </p>
       </Content>
     </Container>
