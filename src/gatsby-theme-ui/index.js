cbb2d4a19f08c8f74175a83dce9d5a5147c0001b
Sienna Luong <74251221+siennaluong@users.noreply.github.com>
2023-06-29 03:41:42 -0400

    change background color

---
+++ src/gatsby-theme-ui/index.js
@@ -0,0 +1,9 @@
+import amsterdamTheme from 'gatsby-theme-amsterdam/src/gatsby-plugin-theme-ui'
+
+export default {
+  ...amsterdamTheme,
+  colors: {
+    ...amsterdamTheme.colors,
+    background: '#FCD5C0',
+  },
+}
